1a8f9449358d35fca5790c188c0710366c701f2d
Yash Raj <kyashraj991@gmail.com>
2025-12-24 20:04:51 +0530

    feat: add react router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import ImageUpload from "./pages/Image-Upload"
+
 const App = () => {
   return (
-    <div>homeeee page</div>
+   <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<ImageUpload />} />
+      </Routes>
   )
 }
 
