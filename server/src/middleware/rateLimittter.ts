dff4eecfc9d92d6f06b2ae903175dcc428b33287
Yash Raj <kyashraj991@gmail.com>
2025-12-31 13:37:10 +0530

    feat: implement rate limiting for image upload and retrieval routes

---
+++ server/src/middleware/rateLimittter.ts
@@ -0,0 +1,47 @@
+import rateLimit from "express-rate-limit";
+import RedisStore from "rate-limit-redis";
+import { Redis } from "ioredis";
+import type { RedisReply } from "rate-limit-redis";
+
+export const redisClient = new Redis(process.env.REDIS_URL!);
+
+export const createRateLimiter = ({
+  windowMs,
+  max,
+  prefix,
+}: {
+  windowMs: number;
+  max: number;
+  prefix: string;
+}) =>
+  rateLimit({
+    store: new RedisStore({
+      sendCommand: (...args: [string]) =>
+        redisClient.call(...args) as Promise<RedisReply>,
+      prefix,
+    }),
+    windowMs,
+    max,
+    standardHeaders: true,
+    legacyHeaders: false,
+  });
+
+
+export const uploadRateLimiter = createRateLimiter({
+  windowMs: 10 * 60 * 1000,
+  max: 10,
+  prefix: "upload:",
+});
+
+
+export const imagesListLimiter = createRateLimiter({
+  windowMs: 10 * 60 * 1000,
+  max: 15,
+  prefix: "images_list:",
+});
+
+export const imageViewLimiter = createRateLimiter({
+  windowMs: 10 * 60 * 1000,
+  max: 20,
+  prefix: "image_view:",
+});
--- server/src/routes/images.ts
+++ server/src/routes/images.ts
@@ -3,12 +3,23 @@ import { uploadImage } from "../controller/upload";
 import { ProtectRoute } from "../middleware/auth";
 import { upload } from "../middleware/multer";
 import { getImageById, getImages, transfromImage } from "../controller/images";
+import {
+  imagesListLimiter,
+  imageViewLimiter,
+  uploadRateLimiter,
+} from "../middleware/rateLimittter";
 
 const imageRoute = express.Router();
 
-imageRoute.post("/upload", ProtectRoute, upload.single("image"), uploadImage);
-imageRoute.get("/images", ProtectRoute, getImages);
-imageRoute.get("/images/:id", ProtectRoute, getImageById);
+imageRoute.post(
+  "/upload",
+  ProtectRoute,
+  uploadRateLimiter,
+  upload.single("image"),
+  uploadImage
+);
+imageRoute.get("/images", ProtectRoute, imagesListLimiter, getImages);
+imageRoute.get("/images/:id", ProtectRoute, imageViewLimiter, getImageById);
 imageRoute.post("/images/:id/transform", ProtectRoute, transfromImage);
 
 export default imageRoute;
