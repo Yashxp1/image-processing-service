bee31469bf718686534aea424e761afd53364b30
Yash Raj <kyashraj991@gmail.com>
2026-01-01 21:05:09 +0530

    feat: add authentication check route and fix logout route definition

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,12 +1,14 @@
 import express from "express";
-import { login, logout, register } from "../controller/auth";
+import { isLoggedIn, login, logout, register } from "../controller/auth";
 
 const authRoute = express.Router();
 
 
 authRoute.post('/register', register)
 authRoute.post('/login', login)
-authRoute.post('/login', logout)
+authRoute.post('/logout', logout)
+
+authRoute.get('/check-auth', isLoggedIn)
 
 
 export default authRoute;
