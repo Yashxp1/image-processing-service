65b37d64a7c4ee6c190a3a287878a402e3d2b0ea
Yash Raj <kyashraj991@gmail.com>
2026-01-01 21:05:45 +0530

    feat: update JWT cookie settings for improved security and expiration

--- server/src/lib/token.ts
+++ server/src/lib/token.ts
@@ -7,12 +7,19 @@ export const generateToken = (userId: string, res: Response) => {
       expiresIn: "1d",
     });
 
+    // res.cookie("jwt", token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === "production",
+    //   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    //   maxAge: 24 * 60 * 60 * 1000,
+    // });
+
     res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 24 * 60 * 60 * 1000,
-    });
+    httpOnly: true,
+    sameSite: "lax",   
+    secure: false,     
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
     
   } catch (error) {
     console.log(error);
